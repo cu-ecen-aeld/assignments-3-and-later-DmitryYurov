3b6e3549c64d525599792cb683fe294246e21df6
Dmitry Yurov <dm.yurov@gmail.com>
2024-01-26 18:54:57 +0100

    Try fixing the build

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -9,13 +9,13 @@ int main(int argc, char** argv) {
     openlog(argv[0], LOG_CONS, LOG_USER);
     if (argc != 3) {
         syslog(LOG_ERR, "Wrong number of arguments: %d\n", argc);
-        return 1;
+        return 0;
     }
 
     fptr = fopen(argv[1], "a");
     if (fptr == NULL) {
         syslog(LOG_ERR, "Failed to open the file with path %s, error number: %s\n", argv[1], strerror(errno));
-        return 1;
+        return 0;
     }
 
     syslog(LOG_DEBUG, "Writing message %s to the file %s\n", argv[2], argv[1]);
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     if (errno != 0) {
         syslog(LOG_ERR, "Failed to write into the file, reason: %s\n", strerror(errno));
         fclose(fptr);
-        return 1;
+        return 0;
     }
 
     fclose(fptr);
