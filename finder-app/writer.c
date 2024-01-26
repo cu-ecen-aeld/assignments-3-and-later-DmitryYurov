f1a31f03c6cbeda6e3472621af804363b66336d3
Dmitry Yurov <dm.yurov@gmail.com>
2024-01-26 19:13:26 +0100

    Debugging...

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,26 +5,27 @@
 
 int main(int argc, char** argv) {
     FILE* fptr = NULL;
+    int last_error = 0;
 
     openlog(argv[0], LOG_CONS, LOG_USER);
     if (argc != 3) {
+        last_error = 1;
         syslog(LOG_ERR, "Wrong number of arguments: %d\n", argc);
-        return 1;
+        return last_error;
     }
 
     fptr = fopen(argv[1], "a");
     if (fptr == NULL) {
-        syslog(LOG_ERR, "Failed to open the file with path %s, error number: %s\n", argv[1], strerror(errno));
-        return 1;
+        last_error = errno;
+        syslog(LOG_ERR, "Failed to open the file with path %s, error: %s\n", argv[1], strerror(errno));
+        return last_error;
     }
 
     syslog(LOG_DEBUG, "Writing message %s to the file %s\n", argv[2], argv[1]);
-    fprintf(fptr, "%s\n", argv[2]);
 
-    if (errno != 0) {
+    if (fprintf(fptr, "%s\n", argv[2]) < 0) {
+        last_error = errno;
         syslog(LOG_ERR, "Failed to write into the file, reason: %s\n", strerror(errno));
-        fclose(fptr);
-        return 1;
     }
 
     fclose(fptr);
